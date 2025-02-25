a04b17b1829fdd1c4527c1c8a280d6d0296c0e51
Auberon Lopez <auberon.lopez@greenriver.edu>
2025-02-25 08:14:53 -0800

    Added instructions doc and TruffulaOptions skeleton

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # truffula
+
+An assignment for printing colorful file trees. Please see [instructions.md](instructions.md) for instructions.
---
+++ src/TruffulaOptions.java
@@ -0,0 +1,47 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+
+public class TruffulaOptions  {
+  private final File root;
+  private final boolean showHidden;
+  private final boolean useColor;
+
+  public File getRoot() {
+    return root;
+  }
+
+
+  public boolean isShowHidden() {
+    return showHidden;
+  }
+
+
+  public boolean isUseColor() {
+    return useColor;
+  }
+
+
+  // Structure [-h] [-nc] path
+  // -h show hidden files (defaults to false)
+  // -nc do not use color (uses color by default)
+  // Throws IllegalArgumentException if unkown arguments or missing filepath
+  // Throws FileNotFoundException
+  // Examples:
+  // ['-nc', '-h' '/path/to/file'] don't use color, do show hidden files
+  // ['-h', '-nc' '/path/to/file'] don't use color, do show hidden files (order of flags unimportant)
+  // ['/path/to/file'] use color, don't show hidden files
+  public TruffulaOptions (String[] args) throws IllegalArgumentException, FileNotFoundException  {
+    // TODO: Replace the below lines with your implementation
+    root = null;
+    showHidden = false;
+    useColor = false;
+  }
+
+
+  public TruffulaOptions (File root, boolean showHidden, boolean useColor) {
+    // You do not need to modify this constructor
+    this.root = root;
+    this.showHidden = showHidden;
+    this.useColor = useColor;
+  }
+}
