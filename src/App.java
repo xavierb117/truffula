c642e5c0281dab45b31eaeb328c360ec8cb294fe
xavierb <194048107+xavierb117@users.noreply.github.com>
2025-05-19 03:17:09 -0700

    Initial solution for wave 3 passing all messsages

--- src/App.java
+++ src/App.java
@@ -45,5 +45,8 @@ public class App {
     // You should create a TruffulaOptions object using the args and
     // pass it to a new TruffulaPrinter that uses System.out
     // Then, call printTree on the TruffulaPrinter
+    TruffulaOptions chosenOptions = new TruffulaOptions(args);
+    TruffulaPrinter printFiles = new TruffulaPrinter(chosenOptions);
+    printFiles.printTree();
   }
 }
